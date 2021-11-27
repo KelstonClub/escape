504d123ebc1488baa009b2dd038b0b51b10dff05
Tim Golden <tim@kelston.org.uk>
2021-11-27 11:00:04 +0000

    Changes to relay

---
+++ test-relay.py
@@ -0,0 +1,5 @@
+import gpiozero
+
+relay1 = gpiozero.OutputDevice(21, active_high=True, initial_value=False)
+relay2 = gpiozero.OutputDevice(20, active_high=True, initial_value=False)
+relay3 = gpiozero.OutputDevice(26, active_high=True, initial_value=False)
