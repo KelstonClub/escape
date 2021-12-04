92f0751f0d095c51b66ed2e5704f26b0be03aabb
Peter Masini <petermasini@Kelston.org.uk>
2021-12-04 16:56:45 +0000

    Stairway is working

--- stairway.py
+++ stairway.py
@@ -2,27 +2,45 @@ import os, sys
 import logging
 logger = logging.getLogger("stairway")
 import time
+import gpiozero
 
 import networkzero as nw0
 
+relay1 = gpiozero.OutputDevice(21, active_high=True, initial_value=False)
+relay2 = gpiozero.OutputDevice(20, active_high=True, initial_value=False)
+relay3 = gpiozero.OutputDevice(26, active_high=True, initial_value=False)
+
 def reset():
-    raise NotImplementedError
+    handle_primary("on")
+    handle_secondary("off")
 
 def handle_primary(target_state):
     logger.info("About to handle primary %s", target_state)
+    if target_state == "on":
+        relay1.on()
+        relay2.on()
+    else:
+        relay1.off()
+        relay2.off()
     time.sleep(0.2)
 
 def handle_secondary(target_state):
     logger.info("About to handle secondary %s", target_state)
+    if target_state == "on":
+        relay3.on()
+    else:
+        relay3.off()
     time.sleep(0.2)
 
 def handle_message(message):
     logger.info("Handle message %s", message)
-    (zone, state) = message
 
     if message == "reset":
         reset()
-    elif zone == "primary":
+        return
+
+    (zone, state) = message
+    if zone == "primary":
         handle_primary(state)
     elif zone == "secondary":
         handle_secondary(state)
@@ -30,6 +48,7 @@ def handle_message(message):
         raise RuntimeError("Unrecognised message: %s" % message)
 
 def main():
+    reset()
     stairway = nw0.advertise("stairway")
     logger.info("Advertising stairway as %s", stairway)
 
--- test-relay.py
+++ test-relay.py
@@ -1,5 +1,14 @@
+from RPi import GPIO
+def cleanup(*args): print("Cleanup in progress")
+GPIO.cleanup = cleanup
 import gpiozero
 
-relay1 = gpiozero.OutputDevice(21, active_high=False, initial_value=False)
-relay2 = gpiozero.OutputDevice(20, active_high=False, initial_value=False)
-relay3 = gpiozero.OutputDevice(26, active_high=False, initial_value=False)
+relay1 = gpiozero.OutputDevice(21, active_high=True, initial_value=False)
+relay2 = gpiozero.OutputDevice(20, active_high=True, initial_value=False)
+relay3 = gpiozero.OutputDevice(26, active_high=True, initial_value=False)
+
+input("Press enter")
+
+relays = [relay1, relay2, relay3]
+for relay in relays:
+    relay.on()
