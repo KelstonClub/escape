94bf3cb7ab7c8acc9461ff621924650dc065b127
Tim Golden <tim@kelston.org.uk>
2021-12-10 18:35:44 +0000

    Changes from Chus

--- test-keypad2.py
+++ test-keypad2.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
-L1 = 12
-L2 = 16
-L3 = 20
-L4 = 21
+keypad_solution = "A86#3"
 
-C1 = 6
-C2 = 13
-C3 = 19
-C4 = 26
+L1 = 5
+L2 = 6
+L3 = 19
+L4 = 13
+
+C1 = 16
+C2 = 12
+C3 = 25
+C4 = 24
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -26,23 +28,57 @@ GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def readLine(line, characters):
     GPIO.output(line, GPIO.HIGH)
+    char_pressed = ""
     if(GPIO.input(C1) == 1):
-        print(characters[0])
+        char_pressed = characters[0]
     if(GPIO.input(C2) == 1):
-        print(characters[1])
+        char_pressed = characters[1]
     if(GPIO.input(C3) == 1):
-        print(characters[2])
+        char_pressed = characters[2]
     if(GPIO.input(C4) == 1):
-        print(characters[3])
+        char_pressed = characters[3]
     GPIO.output(line, GPIO.LOW)
+    return char_pressed
+
+current_code = ""
+last_char = ""
+
+lines = [ (L1, ["1","2","3","A"]),
+          (L2, ["4","5","6","B"]),
+          (L3, ["7","8","9","C"]),
+          (L4, ["*","0","#","D"]) ]
+
+start_time = time.time()
+print(start_time)
 
 try:
     while True:
-        readLine(L1, ["1","2","3","A"])
-        readLine(L2, ["4","5","6","B"])
-        readLine(L3, ["7","8","9","C"])
-        readLine(L4, ["*","0","#","D"])
+        for i in range(4):
+            new_char = readLine(lines[i][0], lines[i][1])
+            if new_char:
+                if last_char != new_char:
+                    print("valid: {}".format(new_char))
+                    current_code += new_char
+                    last_char = new_char
+                    start_time = time.time()
+                continue
+        
+        if len(current_code) == 5:
+            if current_code == keypad_solution:
+                print("correct!")
+            else:
+                print("wrong!")
+
+            current_code = ""
+        
         time.sleep(0.1)
+        
+        print(len(current_code))
+        if (time.time() - start_time > 5) and len(current_code) > 0:
+            print(time.time() - start_time)
+            print("reset")
+            current_code = ""
+
 except KeyboardInterrupt:
     print("\nApplication stopped!")
 
