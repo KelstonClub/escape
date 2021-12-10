f7932c551e0aeadf54346c57554363241759fa60
Tim Golden <mail@timgolden.me.uk>
2021-12-10 18:35:03 +0000

    Changes from Chus

--- numpad-and-lock.py
+++ numpad-and-lock.py
@@ -1,27 +1,183 @@
-NODE = "numpad"
-
-class _DummyRelay:
-    def on(self): pass
-    def off(self): pass
-
 import os, sys
 import logging
 logger = logging.getLogger("numpad")
-import threading
 import time
-try:
-    import gpiozero
-except ImportError:
-    logger.warn("Can't import gpiozero; probably not on RPi")
-    relay1 = relay2 = relay3 = _DummyRelay()
-else:
-    relay1 = gpiozero.OutputDevice(21, active_high=True, initial_value=False)
-    relay2 = gpiozero.OutputDevice(20, active_high=True, initial_value=False)
-    relay3 = gpiozero.OutputDevice(26, active_high=True, initial_value=False)
+
+import RPi.GPIO as GPIO
+import gpiozero
 
 from pygame import mixer
 mixer.init()
 import networkzero as nw0
+
+MUSIC_FOLDER = "/home/pi/escape/background-sounds"
+
+music = { "base" : "general_soundtrack_knives_out.wav",
+          "intense" : "intense_knives_out.wav"}
+
+ambience = { "general" : "ambience.wav",
+             "wind": "general_soundtrack_knives_out.wav",
+             "mice": "mice.wav" }
+
+keypad_solution = "A86#3"
+
+class relay_ctrl:
+    relays_initialized = False
+    keypad_lock = None
+    black_light = None
+    normal_light = None
+
+    def __init__(self):
+        self.init_relay_system()
+
+    def init_relay_system(self):
+        # loading GPIO and clean up
+        def cleanup(*args): print("Cleanup of relay pins")
+        GPIO.cleanup = cleanup
+
+        # initilize relays
+        # False: open
+        self.keypad_lock = gpiozero.OutputDevice(21, active_high=True, initial_value=True)
+        
+        # False: on
+        self.black_light = gpiozero.OutputDevice(20, active_high=True, initial_value=True)
+
+        # True: on
+        self.normal_light = gpiozero.OutputDevice(26, active_high=True, initial_value=True)
+
+        self.relays_initialized = True
+
+    def switch_keypad_lock(self, value):
+        if value:
+            self.keypad_lock.on()
+        else:
+            self.keypad_lock.off()
+
+    def switch_black_light(self, value):
+        if value:
+            self.black_light.on()
+        else:
+            self.black_light.off()
+
+    def switch_normal_light(self, value):
+        if value:
+            self.normal_light.on()
+        else:
+            self.normal_light.off()
+
+class keypad_ctrl:
+    L1 = 5
+    L2 = 6
+    L3 = 19
+    L4 = 13
+
+    C1 = 16
+    C2 = 12
+    C3 = 25
+    C4 = 24
+
+    lines = [ (L1, ["1","2","3","A"]),
+              (L2, ["4","5","6","B"]),
+              (L3, ["7","8","9","C"]),
+              (L4, ["*","0","#","D"]) ]
+
+    def __init__(self, solution):
+        GPIO.setwarnings(False)
+        # general initialization?
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.setup(self.L1, GPIO.OUT)
+        GPIO.setup(self.L2, GPIO.OUT)
+        GPIO.setup(self.L3, GPIO.OUT)
+        GPIO.setup(self.L4, GPIO.OUT)
+
+        GPIO.setup(self.C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        self.solution = solution
+        self.current_code = ""
+        self.start_time = time.time()
+        self.last_char = ""
+
+        self.valid_key_sound = mixer.Sound("beep.wav")
+        self.valid_key_sound.set_volume(0.65)
+
+        self.wrong_password_sound = mixer.Sound("wrong.wav")
+        self.wrong_password_sound.set_volume(0.4)
+
+        self.correct_password_sound = mixer.Sound("success.wav")
+        self.correct_password_sound.set_volume(0.4)
+
+    def readLine(self, line, characters):
+        GPIO.output(line, GPIO.HIGH)
+        char_pressed = ""
+        if(GPIO.input(self.C1) == 1):
+            char_pressed = characters[0]
+        if(GPIO.input(self.C2) == 1):
+            char_pressed = characters[1]
+        if(GPIO.input(self.C3) == 1):
+            char_pressed = characters[2]
+        if(GPIO.input(self.C4) == 1):
+            char_pressed = characters[3]
+
+        GPIO.output(line, GPIO.LOW)
+        return char_pressed
+
+    def refresh_keypad(self):
+        for i in range(4):
+            new_char = self.readLine(self.lines[i][0], self.lines[i][1])
+            if new_char:
+                if self.last_char != new_char:
+                    print("Valid character: {}".format(new_char))
+                    self.valid_key_sound.play()
+                    self.current_code += new_char
+                    self.last_char = new_char
+                    self.start_time = time.time()
+                continue
+        
+        if len(self.current_code) == 5:
+            if self.current_code == self.solution:
+                self.correct_password_sound.play()
+                self.current_code = ""
+                time.sleep(1)
+                return True
+            else:
+                self.wrong_password_sound.play()
+                self.last_char = ""
+
+            self.current_code = ""
+        
+        time.sleep(0.1)
+        
+        if (time.time() - self.start_time > 5) and len(self.current_code) > 0:
+            self.wrong_password_sound().play()
+            print("Doing a reset")
+            self.current_code = ""
+            self.last_char = ""
+
+        return False
+
+
+def set_athmosphere_phase1(relays):
+    play_music(music["base"])
+    
+    relays.switch_normal_light(True)
+    relays.switch_black_light(True)
+    relays.switch_keypad_lock(True)
+
+def set_athmosphere_phase2(relays):
+    play_music(music["intense"])
+    relays.switch_normal_light(False)
+    relays.switch_black_light(False)
+    time.sleep(1)
+    relays.switch_keypad_lock(False)
+
+def set_stable(relays):
+    relays.switch_normal_light(True)
+    relays.switch_black_light(True)
+    relays.switch_keypad_lock(True)    
 
 def reset():
     logger.info("About to reset...")
@@ -44,33 +200,47 @@ def unlock():
     logger.info("About to unlock...")
     time.sleep(0.2)
 
-def play_success():
-    mixer.Sound("success.wav").play()
+def play_ambience():
+    mixer.Sound("/home/pi/escape/background-sounds/ambience.wav")
 
-def play_error():
-    mixer.Sound("failure.wav").play()
-
-def send_heartbeat():
-    while True:
-        #
-        # Advertise the heartbeat frequently to allow for beacon shutdowns
-        #
-        heartbeat = nw0.advertise("heartbeat/%s" % NODE, HEARTBEAT_ADDRESS)
-        print("Sending heartbeat", NODE, "to", heartbeat)
-        nw0.send_news_to(heartbeat, NODE)
-        time.sleep(HEARTBEAT_INTERVAL_S)
+# stop the previous music and run this one
+def play_music(music_track):
+    mixer.music.load(os.path.join(MUSIC_FOLDER, music_track))
+    mixer.music.set_volume(0.75)
+    mixer.music.play()
 
 def main():
-    reset()
+    # initialize services
     numpad = nw0.advertise("numpad")
-    logger.info("Advertising numpad as %s", numpad)
-    #~ threading.Thread(target=send_heartbeat, daemon=True).start()
+
     logger.info("Looking for stairway...")
-    stairway = nw0.discover("stairway")
-    logger.info("Found stairway at %s", stairway)
+    #stairway = nw0.discover("stairway")
+    #logger.info("Found stairway at %s", stairway)
+
     logger.info("Looking for passageway...")
-    passageway = nw0.discover("passageway")
-    logger.info("Found passageway at %s", passageway)
+    #passageway = nw0.discover("passageway")
+    #logger.info("Found passageway at %s", passageway)
+
+    keypad = keypad_ctrl(keypad_solution)
+    relays = relay_ctrl()
+    set_athmosphere_phase1(relays)
+
+    #init_relay_system()
+    #set_athmosphere_phase1()
+
+    try:
+        while True:
+            if keypad.refresh_keypad():
+                break
+
+        set_athmosphere_phase2(relays)
+        while True:
+            time.sleep(10)
+
+    except KeyboardInterrupt:
+        set_stable(relays)
+        print("\nApplication stopped!")
+        exit(0)
 
     while True:
         logger.info("Checking for message...")
